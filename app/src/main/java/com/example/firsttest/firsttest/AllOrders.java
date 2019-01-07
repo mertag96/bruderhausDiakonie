1623277c214c1eed99fcba6829cbec8407ad42aa
mertag96 <merve.tagmut@student.reutlingen-university.de>
2019-01-07 10:14:35 +0100

    Dialog Fenster etc.

--- app/src/main/java/com/example/firsttest/firsttest/AllOrders.java
+++ app/src/main/java/com/example/firsttest/firsttest/AllOrders.java
@@ -1,19 +1,25 @@
 package com.example.firsttest.firsttest;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
 public class AllOrders extends AppCompatActivity {
 
-    private Button fromDate, toDate;
+    private Button fromDate, toDate, edit, back;
     private TextView fromTxtView, toTxtView;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,8 @@ public class AllOrders extends AppCompatActivity {
 
         toDate = findViewById(R.id.toDate);
         toTxtView = findViewById(R.id.totvSelectedDate);
+        edit = findViewById(R.id.bearbeiten);
+        back = findViewById(R.id.zuruck);
 
         //Calender for the first Button to select "Von"
         fromDate.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +74,50 @@ public class AllOrders extends AppCompatActivity {
                         }, year, month, dayOfMonth);
                 datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
                 datePickerDialog.show();
+            }
+        });
+
+        //back button
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AllOrders.this, Menunavigation.class));
+            }
+        });
+
+        //edit button
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                //set title
+                alertDialogBuilder.setTitle("Hinweis");
+
+                //set Dialog Message
+                alertDialogBuilder.setMessage("Wie möchten Sie vorgehen?").setCancelable(false)
+                        .setPositiveButton("Löschen", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //if this button is clicked, close currect activity
+
+                                //ToDo: Implement to delete correctly and officialy
+                                Toast.makeText(getApplicationContext(), "Erfolgreich gelöscht.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //if this button is clicked, just close the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+                //create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                //show it
+                alertDialog.show();
+
             }
         });
     }
