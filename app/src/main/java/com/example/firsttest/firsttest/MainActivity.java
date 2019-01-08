c477470208b065aabcde961fa5965e05cc239034
mertag96 <merve.tagmut@student.reutlingen-university.de>
2019-01-08 21:50:15 +0100

    Last Updates about the Dropdown fields - Spinner - Functionalities

--- app/src/main/java/com/example/firsttest/firsttest/MainActivity.java
+++ app/src/main/java/com/example/firsttest/firsttest/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.firsttest.firsttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.widget.Button;
-import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
