43b150bbf73afc90ec3243d33366366f59a7d6ad
mertag96 <merve.tagmut@student.reutlingen-university.de>
2019-01-12 16:22:54 +0100

    Implement: after klicking on logout button, there comes now a pop up..

--- app/src/main/java/com/example/firsttest/firsttest/Menunavigation.java
+++ app/src/main/java/com/example/firsttest/firsttest/Menunavigation.java
@@ -1,17 +1,22 @@
 package com.example.firsttest.firsttest;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Menunavigation extends AppCompatActivity {
     ImageView newUser, allUser, menuplan, allOrders;
     ImageButton imgB;
     Button logVisible;
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,8 +75,45 @@ public class Menunavigation extends AppCompatActivity {
                     logVisible.setVisibility(View.GONE);
                 } else {
                     logVisible.setVisibility(View.VISIBLE);
+
+                    logVisible.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                            //set title
+                            alertDialogBuilder.setTitle("Hinweis");
+
+                            //set Dialog Message
+                            alertDialogBuilder.setMessage("Möchten Sie sich ausloggen?").setCancelable(false)
+                                    .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            //if this button is clicked, close currect activity
+                                            //ToDo: implementieren, dass die eingegebenen Daten dann tatsächlich gespeichert werden
+                                            startActivity(new Intent(Menunavigation.this, MainActivity.class));
+                                            Toast.makeText(getApplicationContext(), "Benutzer wurde erfolgreich abgemeldet", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //if this button is clicked, just close the dialog box and do nothing
+                                    dialog.cancel();
+                                }
+                            });
+
+                            //create alert dialog
+                            AlertDialog alertDialog = alertDialogBuilder.create();
+
+                            //show it
+                            alertDialog.show();
+                        }
+                    });
+
                 }
             }
+
+
         });
     }
 }
