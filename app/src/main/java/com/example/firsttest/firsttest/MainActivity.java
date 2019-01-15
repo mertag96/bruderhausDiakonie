3d799d493fc3b632a3a6ed1bbbb9eafa74bfc23f
Seyma Yüksel <seyma.yueksel@student.reutlingen-university.de>
2019-01-15 16:43:23 +0100

    Login to app done.

--- app/src/main/java/com/example/firsttest/firsttest/MainActivity.java
+++ app/src/main/java/com/example/firsttest/firsttest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.firsttest.firsttest;
 
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,6 +12,25 @@ import android.view.View.OnClickListener;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,7 +75,9 @@ public class MainActivity extends AppCompatActivity {
 
 
                     }else{
+                        login();
                         //Loading Panel wird hier erst angezeigt, bis es zur nächsten Activity übergegangen wird
+                        /*
                         if(permission == 1){
                             goToMenuNavi = new Intent(MainActivity.this, Menunavigation.class);
                             goToMenuNavi.putExtra("permission",permission);
@@ -72,8 +95,52 @@ public class MainActivity extends AppCompatActivity {
                         }else{
                             //go to menuplan
                             //add permission as extras to intent
+                        }*/
+                }
                         }
                     }
             }});
+    }
+
+    public void login() {
+        NetworkHandler networkHelper = new NetworkHandler();
+        JSONObject json = new JSONObject();
+        try {
+            json.put("username", username.getText());
+            json.put("password", password.getText());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        networkHelper.post("http://134.103.176.137:8081/login", json.toString(), new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+            }
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String responseStr = response.body().string();
+                System.out.println("HALLOOO"+responseStr.length());
+
+                if(responseStr.length()<=2){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Login fehlerhaft!", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }else{
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            goToMenuNavi = new Intent(MainActivity.this, Menunavigation.class);
+                            goToMenuNavi.putExtra("permission",permission);
+                            loading.setVisibility(View.VISIBLE);
+                            startActivity(goToMenuNavi);
+                            loading.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                }
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/firsttest/firsttest/NetworkHandler.java
@@ -0,0 +1,26 @@
+package com.example.firsttest.firsttest;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+
+public class NetworkHandler {
+    public static final MediaType JSON = MediaType.parse("application/json");
+
+    OkHttpClient client = new OkHttpClient();
+
+    Call post(String url, String json, Callback callback) {
+        RequestBody body = RequestBody.create(JSON, json);
+        System.out.println(body);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+}
