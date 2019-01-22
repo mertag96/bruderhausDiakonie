c400154a96390e6dbfe60e35fe2beebe8e132ce5
Seyma Yüksel <seyma.yueksel@student.reutlingen-university.de>
2019-01-22 22:06:05 +0100

    Yasins branch: tabs with tables added

--- app/src/main/java/com/example/firsttest/firsttest/Tab2Fragment.java
+++ app/src/main/java/com/example/firsttest/firsttest/Tab2Fragment.java
@@ -1,11 +1,16 @@
 package com.example.firsttest.firsttest;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class Tab2Fragment extends Fragment {
     private static final String TAG = "Tab2Fragment";
@@ -14,6 +19,50 @@ public class Tab2Fragment extends Fragment {
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.tab2_fragment, container, false);
+
+        TableLayout stk = (TableLayout) view.findViewById(R.id.table_main2);
+        TableRow tbrow0 = new TableRow(getActivity());
+        TextView tv0 = new TextView(getActivity());
+        tv0.setText(" Datum ");
+        tv0.setTextColor(Color.WHITE);
+        tbrow0.addView(tv0);
+        TextView tv1 = new TextView(getActivity());
+        tv1.setText(" Menü ");
+        tv1.setTextColor(Color.WHITE);
+        tbrow0.addView(tv1);
+        TextView tv2 = new TextView(getActivity());
+        tv2.setText(" Gericht ");
+        tv2.setTextColor(Color.WHITE);
+        tbrow0.addView(tv2);
+        TextView tv3 = new TextView(getActivity());
+        tv3.setText(" Quantität ");
+        tv3.setTextColor(Color.WHITE);
+        tbrow0.addView(tv3);
+        stk.addView(tbrow0);
+        for (int i = 0; i < 7; i++) {
+            TableRow tbrow = new TableRow(getActivity());
+            TextView t1v = new TextView(getActivity());
+            t1v.setText("2" + i + "." + "0" + (i+1) + "." + "2019");
+            t1v.setTextColor(Color.WHITE);
+            t1v.setGravity(Gravity.CENTER);
+            tbrow.addView(t1v);
+            TextView t2v = new TextView(getActivity());
+            t2v.setText("" + (i+1));
+            t2v.setTextColor(Color.WHITE);
+            t2v.setGravity(Gravity.CENTER);
+            tbrow.addView(t2v);
+            TextView t3v = new TextView(getActivity());
+            t3v.setText("Product " + (i+1));
+            t3v.setTextColor(Color.WHITE);
+            t3v.setGravity(Gravity.CENTER);
+            tbrow.addView(t3v);
+            TextView t4v = new TextView(getActivity());
+            t4v.setText("" + i * 20 / 32 * 5);
+            t4v.setTextColor(Color.WHITE);
+            t4v.setGravity(Gravity.CENTER);
+            tbrow.addView(t4v);
+            stk.addView(tbrow);
+        }
 
         return view;
     }
--- app/src/main/java/com/example/firsttest/firsttest/allOrdersKitchen.java
+++ app/src/main/java/com/example/firsttest/firsttest/allOrdersKitchen.java
@@ -61,7 +61,7 @@ public class allOrdersKitchen extends AppCompatActivity {
         //Set up the viewPager with the sections adapter
         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.container);
-        //setupViewPager(mViewPager);
+        setupViewPager(mViewPager);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
@@ -136,52 +136,6 @@ public class allOrdersKitchen extends AppCompatActivity {
         adapter.addFragment(new Tab3Fragment(), "Nachtisch");
         viewPager.setAdapter(adapter);
 
-    }
-
-    public void init() {
-        TableLayout stk = (TableLayout) findViewById(R.id.table_main);
-        TableRow tbrow0 = new TableRow(this);
-        TextView tv0 = new TextView(this);
-        tv0.setText(" Datum ");
-        tv0.setTextColor(Color.WHITE);
-        tbrow0.addView(tv0);
-        TextView tv1 = new TextView(this);
-        tv1.setText(" Menü ");
-        tv1.setTextColor(Color.WHITE);
-        tbrow0.addView(tv1);
-        TextView tv2 = new TextView(this);
-        tv2.setText(" Gericht ");
-        tv2.setTextColor(Color.WHITE);
-        tbrow0.addView(tv2);
-        TextView tv3 = new TextView(this);
-        tv3.setText(" Quantität ");
-        tv3.setTextColor(Color.WHITE);
-        tbrow0.addView(tv3);
-        stk.addView(tbrow0);
-        for (int i = 0; i < 3; i++) {
-            TableRow tbrow = new TableRow(this);
-            TextView t1v = new TextView(this);
-            t1v.setText("" + i);
-            t1v.setTextColor(Color.WHITE);
-            t1v.setGravity(Gravity.CENTER);
-            tbrow.addView(t1v);
-            TextView t2v = new TextView(this);
-            t2v.setText("Product " + i);
-            t2v.setTextColor(Color.WHITE);
-            t2v.setGravity(Gravity.CENTER);
-            tbrow.addView(t2v);
-            TextView t3v = new TextView(this);
-            t3v.setText("Rs." + i);
-            t3v.setTextColor(Color.WHITE);
-            t3v.setGravity(Gravity.CENTER);
-            tbrow.addView(t3v);
-            TextView t4v = new TextView(this);
-            t4v.setText("" + i * 15 / 32 * 10);
-            t4v.setTextColor(Color.WHITE);
-            t4v.setGravity(Gravity.CENTER);
-            tbrow.addView(t4v);
-            stk.addView(tbrow);
-        }
     }
 }
 
